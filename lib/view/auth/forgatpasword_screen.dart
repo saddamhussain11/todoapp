3b9b9168f2660a0706fffbd8e488183c0644deb8
saddamhussain11 <saddamkhokher77@gmail.com>
2025-02-07 00:52:10 -0800

    folder structure

---
+++ lib/view/auth/forgatpasword_screen.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class ForgatpaswordScreen extends StatelessWidget {
+  const ForgatpaswordScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
